9fc8de083c82a2afe02e2e6c70a111090e398ab4
I-am-Jaspreet-Sharma <jaspreetsharma501@gmail.com>
2025-06-13 12:02:32 +0530

    INSTALLING I3 WINDOW MANAGER - 03

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,7 @@
 
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.desktopManager.xfce.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
